460c5b717990acd2c5c23be267d0645d20e69efd
Mani <mani.chandra@gmail.com>
2021-05-14 09:38:08 +0530

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World - now - from Mani"}
+		res := &response{Message: "Hello World - now - from Mani on Friday"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
